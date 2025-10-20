c97e973637ee628145025fa3e7823856585961fa
sofiramoss <sosoramos21@gmail.com>
2025-10-20 15:46:00 -0300

    45y

--- variaveis.php
+++ variaveis.php
@@ -13,5 +13,28 @@ echo ($nomeCompleto);
 echo '<hr>';
 $cpf = '1234567890';
 var_dump($cpf);
+echo '<hr>';
 
+if ($cpf == '14715887644') {
+    echo 'Esse cpf ta esquisito';
+}else{
+    echo 'Com esse cpf mais R$20000000000 voçê entra no cinema!!!';
+}
+echo '<hr>';
+function soma($peso, $altura)
+{
+    $imc = $peso / ($altura * $altura);
+    echo 'seu imc é' . $imc . '. ';
 
+    if ($imc < 18) {
+        return 'abaixo do peso';
+    }elseif ($imc > 18 && $imc <= 65) {
+        return 'peso normal';
+    }elseif ($imc > 65 && $imc <= 90) {
+        return 'sobrepeso';
+    }elseif ($imc > 90 && $imc <= 100) {
+        return 'obesidade';
+    }
+}
+
+echo "meu imc é " . soma(55, 1.70);
